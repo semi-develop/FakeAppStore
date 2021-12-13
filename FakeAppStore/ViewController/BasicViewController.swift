ada985a555205aa0da56969d8d02b88257923ec4
semi <sso999115@gmail.com>
2021-12-14 08:04:51 +0900

    Add NaviBar/ TabBar/ BasicVC/ SearchVC

---
+++ FakeAppStore/ViewController/BasicViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BasicViewController.swift
+//  FakeAppStore
+//
+//  Created by user on 2021/12/14.
+//
+
+import UIKit
+
+class BasicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ FakeAppStore/ViewController/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  FakeAppStore
+//
+//  Created by user on 2021/12/14.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
